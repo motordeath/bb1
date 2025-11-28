58b66b2ba510a9f7e28ec0290bc0755832dfef7d
Aditya Bhardwaj <aditya.bdwj2005@gmail.com>
2025-11-28 23:51:24 +0530

    fix: Remove duplicate import in users.py

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -1,11 +1,3 @@
-from flask import Blueprint, jsonify
-from flask import Blueprint, jsonify
-from models import User
-
-users_bp = Blueprint('users', __name__)
-
-@users_bp.route('/similar/<uid>', methods=['GET'])
-def get_similar_users(uid):
     try:
         users = User.get_similar(uid)
         return jsonify(users), 200
